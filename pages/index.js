8ab9243e5216fd5c67e78cfdcf655bbb721f0a97
Matt Baxter <mbx33@hotmail.com>
2022-10-26 16:39:18 -0600

    Initial

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Testing the latest Nextjs</title>
+				<meta name="description" content="Testing the latest" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<h1 className="capitalize text-center text-6xl font-bold">
+					hello, Next.js!
+				</h1>
+			</main>
+		</div>
+	);
+}
